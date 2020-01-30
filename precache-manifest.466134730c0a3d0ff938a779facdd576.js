2011cee13c8876a736b8192a87b6e76098fd6951
김현태 <leso@ziktalk.com>
2020-01-31 04:51:01 +0900

    Updates

---
+++ precache-manifest.466134730c0a3d0ff938a779facdd576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac8333209dc3bcc11c7003e1b14932f",
+    "url": "/plpl/index.html"
+  },
+  {
+    "revision": "4b972a58fa6b40774be6",
+    "url": "/plpl/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9597dd157eb2682bcf21",
+    "url": "/plpl/static/js/2.149f5ce1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/plpl/static/js/2.149f5ce1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b972a58fa6b40774be6",
+    "url": "/plpl/static/js/main.7c9eeec4.chunk.js"
+  },
+  {
+    "revision": "a25d682073e3fedaaf8b",
+    "url": "/plpl/static/js/runtime-main.39d77743.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f1e12f595e1a3c0f7fd1a347962ac45.js"
+  "/plpl/precache-manifest.466134730c0a3d0ff938a779facdd576.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plpl/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
