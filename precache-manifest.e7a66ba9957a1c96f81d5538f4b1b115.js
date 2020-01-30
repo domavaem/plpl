985d66f244dd72c3cc7bf95b9f260808ff8742f1
Kaphitoshka <domavaem@gmail.com>
2020-01-31 05:12:03 +0900

    add all files

---
+++ precache-manifest.e7a66ba9957a1c96f81d5538f4b1b115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388826c33de19ac6ba48890bae1eebf3",
+    "url": "/plpl/index.html"
+  },
+  {
+    "revision": "dc56a0fe51ac918c6243",
+    "url": "/plpl/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0fef7fd768c3230db049",
+    "url": "/plpl/static/js/2.ddb0782a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/plpl/static/js/2.ddb0782a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc56a0fe51ac918c6243",
+    "url": "/plpl/static/js/main.f5eef386.chunk.js"
+  },
+  {
+    "revision": "5c5aa65f7b473775b306",
+    "url": "/plpl/static/js/runtime-main.ebce3bde.js"
+  }
+]);
