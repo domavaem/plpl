194e2f5b9eb661bb04746e8ddc0caf988a05832d
Kaphitoshka <domavaem@gmail.com>
2020-01-31 04:14:20 +0900

    push react app

---
+++ precache-manifest.cf5af3ffc8979e5798dd5ea432b53f2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e981b96ed0ea8bd8935d1fe42f81438d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2a3720a37241bd3bfa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "502525a3491ad0eaae17",
+    "url": "/static/js/2.29e3f1dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29e3f1dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b2a3720a37241bd3bfa",
+    "url": "/static/js/main.ef572638.chunk.js"
+  },
+  {
+    "revision": "29c4995013be4954e500",
+    "url": "/static/js/runtime-main.dcc7b5e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
