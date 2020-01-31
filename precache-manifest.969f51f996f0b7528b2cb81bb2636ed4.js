eac77e1751f726a4778b5aa540fb6f1b6086b32a
Kaphitoshka <domavaem@gmail.com>
2020-01-31 15:32:58 +0900

    change text

---
+++ precache-manifest.969f51f996f0b7528b2cb81bb2636ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a74f4f4f9e3048107bae13a40a35f1",
+    "url": "/plpl/index.html"
+  },
+  {
+    "revision": "1284bc200d9c0bb50623",
+    "url": "/plpl/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0fef7fd768c3230db049",
+    "url": "/plpl/static/js/2.ddb0782a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/plpl/static/js/2.ddb0782a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1284bc200d9c0bb50623",
+    "url": "/plpl/static/js/main.177bced5.chunk.js"
+  },
+  {
+    "revision": "5c5aa65f7b473775b306",
+    "url": "/plpl/static/js/runtime-main.ebce3bde.js"
+  }
+]);
